85300a2533de6ba2b58c197a23a77d8a49fe3fe3
rob <robiniscoding@gmail.com>
2020-11-10 18:31:29 +0100

    [Tasks] T1-18 and T1-08 done !

---
+++ src/bdd/sql-scripts/insertTestData.sql
@@ -0,0 +1,8 @@
+INSERT INTO projects (name, description)
+VALUES (
+    'cdp-g1-eq1',
+    'Projet pour le cours de Conduite de projet'
+), (
+    'projet concour photo',
+    'Projet pour le cours de programmation web'
+);
