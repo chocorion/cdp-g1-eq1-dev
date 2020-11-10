7ccde4810156e9e0aca848537b5335d9e3d7b412
Yuki <alexisperignon@live.fr>
2020-11-10 19:50:11 +0100

    [Tasks] T1-03 done.

--- src/front/src/app/app-routing.module.ts
+++ src/front/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BacklogComponent } from './backlog/backlog.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { PlanificationComponent } from './planification/planification.component';
+import { SprintActifComponent } from './sprint-actif/sprint-actif.component';
+import { TestsComponent } from './tests/tests.component';
+import { ReleasesComponent } from './releases/releases.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  { path: 'backlog'      , component: BacklogComponent },
+  { path: 'tasks'        , component: TasksComponent },
+  { path: 'planification', component: PlanificationComponent },
+  { path: 'sprintActif'  , component: SprintActifComponent },
+  { path: 'tests'        , component: TestsComponent },
+  { path: 'releases'     , component: ReleasesComponent },
+  { path: 'documentation', component: DocumentationComponent },
+  { path: 'statistics'   , component: StatisticsComponent },
+  { path: ''             , redirectTo: '/backlog', pathMatch: 'full'},
+  { path: '**'           , component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/front/src/app/app.module.ts
+++ src/front/src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BacklogComponent } from './backlog/backlog.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { PlanificationComponent } from './planification/planification.component';
+import { SprintActifComponent } from './sprint-actif/sprint-actif.component';
+import { TestsComponent } from './tests/tests.component';
+import { ReleasesComponent } from './releases/releases.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BacklogComponent,
+    TasksComponent,
+    PlanificationComponent,
+    SprintActifComponent,
+    TestsComponent,
+    ReleasesComponent,
+    DocumentationComponent,
+    StatisticsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
