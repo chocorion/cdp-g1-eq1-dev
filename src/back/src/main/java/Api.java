eb575d8c423a1bb3a2432e8742113a8e5e921a2e
rob <robiniscoding@gmail.com>
2020-11-10 12:12:31 +0100

    Add src base files

---
+++ src/back/src/main/java/Api.java
@@ -0,0 +1,10 @@
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api/v1")
+public class Api extends ResourceConfig {
+    public Api() {
+        packages("", "routes");
+    }
+}
