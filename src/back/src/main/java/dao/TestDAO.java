c787842ee4c5121278086257b40d07bc74ea4e64
rob <robiniscoding@gmail.com>
2020-11-18 17:14:04 +0100

    [Refactor] Remove sql dependencies from dao

--- src/back/src/main/java/dao/TestDAO.java
+++ src/back/src/main/java/dao/TestDAO.java
@@ -1,14 +1,12 @@
 package dao;
 
 import domain.Test;
-
-import java.sql.SQLException;
 import java.util.List;
 
 public interface TestDAO {
-    Test getById(int id) throws SQLException;
-    List<Test> getAllForProject(int projectId) throws SQLException;
-    Test addOne(Test test) throws SQLException;
-    void updateOne(Test test) throws SQLException;
-    void deleteOne(Test test) throws SQLException;
+    Test getById(int id) throws Exception;
+    List<Test> getAllForProject(int projectId) throws Exception;
+    Test addOne(Test test) throws Exception;
+    void updateOne(Test test) throws Exception;
+    void deleteOne(Test test) throws Exception;
 }
