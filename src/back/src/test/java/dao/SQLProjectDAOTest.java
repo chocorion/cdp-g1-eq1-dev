2617de6eeafb8dab3d4f5cdbfb06fbfdea2e941f
rob <robiniscoding@gmail.com>
2020-11-20 18:46:03 +0100

    [Tests] Add some tests for project dao

--- src/back/src/test/java/dao/SQLProjectDAOTest.java
+++ src/back/src/test/java/dao/SQLProjectDAOTest.java
@@ -5,13 +5,36 @@ import org.junit.jupiter.api.Test;
 import dao.sql.SQLProjectDAO;
 import domain.Project;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectTest {
+    @Test
+    void testSimpleInsert() {
+        SQLProjectDAO projectDAO = new SQLProjectDAO();
+        Project project = new Project("OCaMl project","I am an OCaMl project");
+
+        assertDoesNotThrow(() -> projectDAO.insert(project));
+    }
 
     @Test
-    void testInsert() throws Exception {
+    void testSimpleInsertTwice() {
+        SQLProjectDAO projectDAO = new SQLProjectDAO();
         Project project = new Project("Java project","I am a java project");
-        new SQLProjectDAO().insert(project);
+
+        assertDoesNotThrow(() -> projectDAO.insert(project));
+        assertThrows(SQLException.class, () -> projectDAO.insert(project));
+    }
+
+    @Test
+    void testInsert() throws SQLException {
+        SQLProjectDAO projectDAO = new SQLProjectDAO();
+        Project project = new Project("Python project","I am a Python project");
+
+        Project insertedProject = projectDAO.insert(project);
+
+        assertEquals(project.name, insertedProject.name);
+        assertEquals(project.description, project.description);
     }
 }
