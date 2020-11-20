bc3d63f24b2d6104586a3465b0ecefde73532066
Adri <adrien.boitelle@gmail.com>
2020-11-20 17:48:42 +0100

    [Test] add test for project insertion

---
+++ src/back/src/test/java/dao/SQLProjectDAOTest.java
@@ -0,0 +1,17 @@
+package dao;
+
+import org.junit.jupiter.api.Test;
+
+import dao.sql.SQLProjectDAO;
+import domain.Project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectTest {
+
+    @Test
+    void testInsert() throws Exception {
+        Project project = new Project("Java project","I am a java project");
+        new SQLProjectDAO().insert(project);
+    }
+}
