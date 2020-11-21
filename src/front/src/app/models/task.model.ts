0a9abbc4437daf1798c45a45247cf8f645372d07
Yuki <alexisperignon@live.fr>
2020-11-21 16:04:26 +0100

    [Task] T2-22 Done

---
+++ src/front/src/app/models/task.model.ts
@@ -0,0 +1,76 @@
+export class Task {
+    private id: number;
+    private projectId: number;
+    private usId: number;
+    private title: string;
+    private duration: string;
+    private status: string;
+
+    constructor(id: number, projectId: number, usId: number, title: string, duration: string, status: string) {
+        this.id = (id) ? id : -1;
+        this.projectId = projectId;
+        this.usId = usId;
+        this.title = title;
+        this.duration = duration;
+        this.status = status;
+    }
+
+    static fromJSON(json): Task {
+        return new Task(
+            json.id,
+            json.projectId,
+            json.usId,
+            json.title,
+            json.duration,
+            json.status
+            );
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    setId(value: number): void {
+        this.id = value;
+    }
+
+    getProjectId(): number {
+        return this.projectId;
+    }
+
+    setProjectId(value: number): void {
+        this.projectId = value;
+    }
+
+    getUsId(): number {
+        return this.usId;
+    }
+
+    setUsId(value: number): void {
+        this.usId = value;
+    }
+
+    getTitle(): string {
+        return this.title;
+    }
+
+    setTitle(value: string): void {
+        this.title = value;
+    }
+
+    getDuration(): string {
+        return this.duration;
+    }
+
+    setDuration(value: string): void {
+        this.duration = value;
+    }
+
+    getStatus(): string {
+        return this.status;
+    }
+
+    setStatus(value: string): void {
+        this.status = value;
+    }
+}
