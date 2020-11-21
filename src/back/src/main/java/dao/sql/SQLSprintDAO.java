bdd8079c067b4ae0397eafb2194a4391b0d4c477
Yuki <alexisperignon@live.fr>
2020-11-21 14:58:05 +0100

    [Task] T2-09 DONE

---
+++ src/back/src/main/java/dao/sql/SQLSprintDAO.java
@@ -0,0 +1,72 @@
+package dao.sql;
+
+import dao.SprintDAO;
+import domain.Sprint;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+public class SQLSprintDAO extends SQLDAO<Sprint> implements SprintDAO {
+    @Override
+    protected Sprint createObjectFromResult(ResultSet resultSet) throws SQLException {
+        return new Sprint(
+            resultSet.getInt("project"),
+            resultSet.getString("name"),
+            resultSet.getInt("id")
+        );
+    }
+
+    @Override
+    public Sprint getById(int projectId, int id) throws SQLException {
+        String statement = "SELECT * FROM sprint WHERE project=? AND id=?";
+        List<Object> opt = Arrays.asList(projectId, id);
+
+        return queryFirstObject(statement, opt);
+    }
+
+    @Override
+    public List<Sprint> getAllForProject(int projectId) throws SQLException {
+        String statement = "SELECT * FROM sprint WHERE project=?";
+        List<Object> opt = Arrays.asList(projectId);
+
+        return queryAllObjects(statement, opt);
+    }
+
+    @Override
+    public Sprint insert(Sprint sprint) throws Exception {
+        if (sprint.id != null)
+            throw new SQLException("This sprint already has an id, use update !");
+        String statement = "INSERT INTO sprint (project, name)";
+        statement += "VALUES (?,?)";
+
+        List<Object> opt = Arrays.asList(
+                sprint.projectId,
+                sprint.name
+        );
+
+        return new Sprint(sprint.projectId, sprint.name, doInsert(statement, opt));
+    }
+
+    @Override
+    public void update(Sprint sprint) throws SQLException {
+        String statement = "UPDATE sprint SET name = ? WHERE project = ? AND id = ?";
+
+        List<Object> opt = Arrays.asList(
+                sprint.name,
+                sprint.projectId,
+                sprint.id
+        );
+
+        SQLDatabase.exec(statement, opt);
+    }
+
+    @Override
+    public void delete(Sprint sprint) throws SQLException {
+        String statement = "DELETE FROM sprint WHERE project = ? AND id = ?";
+        List<Object> opt = Arrays.asList(sprint.projectId, sprint.id);
+
+        SQLDatabase.exec(statement, opt);
+    }
+}
--- src/back/src/test/java/dao/sql/SQLSprintDAOTest.java
+++ src/back/src/test/java/dao/sql/SQLSprintDAOTest.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQLSprintDAOTest {
-    /*
+
     @Test
     void testSimpleInsert() {
         SQLSprintDAO sprintDAO = new SQLSprintDAO();
@@ -18,21 +18,22 @@ class SQLSprintDAOTest {
     }
 
     @Test
-    void testSimpleInsertTwice() {
+    void testSimpleInsertTwice()  {
         SQLSprintDAO sprintDAO = new SQLSprintDAO();
         Sprint sprint = new Sprint(1, "I am a sprint 2");
 
         assertDoesNotThrow(() -> sprintDAO.insert(sprint));
-        assertThrows(SQLException.class, () -> sprintDAO.insert(sprint));
+        assertDoesNotThrow(() -> sprintDAO.insert(sprint));
     }
 
     @Test
-    void testInsert() throws SQLException {
+    void testInsert() throws Exception {
         SQLSprintDAO sprintDAO = new SQLSprintDAO();
         Sprint sprint = new Sprint(1, "I am a sprint 3");
 
         Sprint insertedSprint = sprintDAO.insert(sprint);
 
         assertEquals(sprint.name, insertedSprint.name);
-    }*/
+        assertEquals(sprint.projectId, sprint.projectId);
+    }
 }
