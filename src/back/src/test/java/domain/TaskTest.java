b86a1f5d663ab7528235e88244a146cdd8482e96
Adri <adrien.boitelle@gmail.com>
2020-11-21 22:36:24 +0100

    [Tests] Add tests for US and tasks

---
+++ src/back/src/test/java/domain/TaskTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class TaskTest {
+    @Test
+    void testEqual() {
+        Task t1 = new Task();
+        Task t2 = new Task(1, 1, "super title2", "1h.h", "TODO");
+        Task t3 = new Task(1, 1, "super title2", "1h.h", "TODO");
+        Task t4 = new Task(1, 1, "super title3", "1h.h", "TODO");
+
+        assertNotEquals(t1, t2);
+        assertEquals(t2, t3);
+        assertNotEquals(t3, t4);
+    }
+}
---
+++ src/back/src/test/java/domain/UserStoriesTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class UserStoriesTest {
+    @Test
+    void testEqual() {
+        UserStory u1 = new UserStory();
+        UserStory u2 = new UserStory(1, "US trop bien", "High", 2);
+        UserStory u3 = new UserStory(1, "US trop bien", "High", 2);
+        UserStory u4 = new UserStory(1, "US trop bien", "High", 3);
+
+        assertNotEquals(u1, u2);
+        assertEquals(u2, u3);
+        assertNotEquals(u3, u4);
+    }
+}
