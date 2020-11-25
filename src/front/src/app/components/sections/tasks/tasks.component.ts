df9328e2c2b5e06ba2727e8c0d04df12ce4ab2e4
Yuki <alexisperignon@live.fr>
2020-11-25 14:34:45 +0100

    [WIP] First draw form new task

--- src/front/src/app/components/sections/tasks/tasks.component.ts
+++ src/front/src/app/components/sections/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+  form: any;
+  private formBuilder: FormBuilder = new FormBuilder();
 
   constructor() { }
 
   ngOnInit(): void {
+      this.form = this.formBuilder.group({
+        taskId: 0,
+        title: '',
+        usId: '',
+        duration: '',
+        status: '',
+        parents: '',
+        children: '',
+        member: '',
+    });
+  }
+
+  createTask(data: any): void {
+    console.log("je suis une task");
   }
 
 }
