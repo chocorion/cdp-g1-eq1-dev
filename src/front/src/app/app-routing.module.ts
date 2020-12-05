49b2bbad09e9d42abb1f889907b1f4820c6fa94a
Adri <adrien.boitelle@gmail.com>
2020-12-05 16:56:10 +0100

    [Front] enable anchor scrolling

--- src/front/src/app/app-routing.module.ts
+++ src/front/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
 import { BacklogComponent } from './components/sections/backlog/backlog.component';
 import { TasksComponent } from './components/sections/tasks/tasks.component';
@@ -29,8 +29,13 @@ const routes: Routes = [
   { path: '**'           , component: PageNotFoundComponent}
 ];
 
+const routerOptions: ExtraOptions = {
+  anchorScrolling: 'enabled',
+  scrollPositionRestoration: 'enabled',
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
