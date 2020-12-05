eb1e86337c6cc343d8e9cc7b152b1ba9a97b3e7c
Adri <adrien.boitelle@gmail.com>
2020-12-05 15:29:19 +0100

    [Back] add user stories to release

--- src/back/src/test/java/dao/sql/SQLReleaseDAOTest.java
+++ src/back/src/test/java/dao/sql/SQLReleaseDAOTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import domain.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,7 +14,7 @@ class SQLReleaseDAOTest {
     @Test
     void testSimpleInsertDelete() {
         SQLReleaseDAO releaseDAO = new SQLReleaseDAO();
-        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04");
+        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04", new ArrayList<UserStory>());
 
         assertDoesNotThrow(() -> releaseDAO.delete(releaseDAO.insert(release)));
     }
@@ -20,7 +22,7 @@ class SQLReleaseDAOTest {
     @Test
     void testSimpleInsertTwice() throws SQLException {
         SQLReleaseDAO releaseDAO = new SQLReleaseDAO();
-        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04");
+        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04", new ArrayList<UserStory>());
 
         Release inserted = assertDoesNotThrow(() -> releaseDAO.insert(release));
         releaseDAO.delete(assertDoesNotThrow(() -> releaseDAO.insert(release)));
@@ -30,7 +32,7 @@ class SQLReleaseDAOTest {
     @Test
     void testInsert() throws SQLException {
         SQLReleaseDAO releaseDAO = new SQLReleaseDAO();
-        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04");
+        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04", new ArrayList<UserStory>());
 
         Release insertedRelease = releaseDAO.insert(release);
 
@@ -45,16 +47,31 @@ class SQLReleaseDAOTest {
     @Test
     void testUpdate() throws Exception {
         SQLReleaseDAO releaseDAO = new SQLReleaseDAO();
-        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04");
+
+        SQLUserStoryDAO userStoryDAO = new SQLUserStoryDAO();
+        UserStory us = new UserStory(1, "US trop bien", "High", 2, null);
+        UserStory insertedUs = userStoryDAO.insert(us);
+
+        Release release = new Release(1, "test", "desc", new Version(1, 2, 3), "testest", "2020-12-04", Arrays.asList(insertedUs));
 
         Release insertedRelease = releaseDAO.insert(release);
 
-        Release updateRelease = new Release(1, "test2", "desc2", new Version(12, 22, 32), "testest2", "2020-12-05", insertedRelease.id);
-        releaseDAO.update(updateRelease);
+        Release updateRelease = new Release(1, "test2", "desc2", new Version(12, 22, 32), "testest2", "2020-12-05", new ArrayList<UserStory>(), insertedRelease.id);
+        assertDoesNotThrow(() -> releaseDAO.update(updateRelease));
 
         Release updatedRelease = releaseDAO.getById(updateRelease.id);
 
         assertTrue(updatedRelease.equals(updateRelease));
-        assertDoesNotThrow(() -> releaseDAO.delete(updatedRelease));
+        assertTrue(updatedRelease.userStories.isEmpty());
+
+        Release updateRelease2 = new Release(1, "test2", "desc2", new Version(12, 22, 32), "testest2", "2020-12-05", Arrays.asList(insertedUs), insertedRelease.id);
+        assertDoesNotThrow(() -> releaseDAO.update(updateRelease2));
+
+        Release updatedRelease2 = releaseDAO.getById(updateRelease2.id);
+        assertTrue(updatedRelease2.equals(updateRelease2));
+        assertTrue(updatedRelease2.userStories.get(0).equals(insertedUs));
+
+        assertDoesNotThrow(() -> releaseDAO.delete(updatedRelease2));
+        assertDoesNotThrow(() -> userStoryDAO.delete(insertedUs));
     }
 }
