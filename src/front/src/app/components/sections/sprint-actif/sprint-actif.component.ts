55bd896a7827deb4a023b2df43befb8a0d46203c
Yuki <alexisperignon@live.fr>
2020-12-05 16:49:26 +0100

    [Task] T3-03 DONE

--- src/front/src/app/components/sections/sprint-actif/sprint-actif.component.ts
+++ src/front/src/app/components/sections/sprint-actif/sprint-actif.component.ts
@@ -5,28 +5,33 @@ import { ProjectService } from 'src/app/services/project.service';
 import { SprintService } from 'src/app/services/sprint.service';
 
 @Component({
-  selector: 'app-sprint-actif',
-  templateUrl: './sprint-actif.component.html',
-  styleUrls: ['./sprint-actif.component.css']
+    selector: 'app-sprint-actif',
+    templateUrl: './sprint-actif.component.html',
+    styleUrls: ['./sprint-actif.component.css']
 })
 export class SprintActifComponent implements OnInit {
 
-  userStories: Us[] = [];
-  sprint: Sprint;
+    userStories: Us[] = [];
+    sprint: Sprint;
 
-  constructor(private sprintService: SprintService,
-              private projectService: ProjectService) { }
+    constructor(private sprintService: SprintService,
+                private projectService: ProjectService) { }
 
-  ngOnInit(): void {
-    this.sprintService.getActiveSprint().subscribe(
-      sprint => {
-      this.sprint = sprint;
-      this.sprintService.getUs(this.projectService.currentProject.getId(), this.sprint.getId()).subscribe(
-            usList => {
-                usList.forEach(us => this.userStories.push(Us.fromJSON(us)));
-            }
-        );
-      });
-  }
+    ngOnInit(): void {
+        this.sprintService.getActiveSprint().subscribe(
+            sprint => {
+                this.sprint = sprint;
+                this.sprintService.getUs(this.projectService.currentProject.getId(), this.sprint.getId()).subscribe(
+                    usList => {
+                        usList.forEach(us => this.userStories.push(Us.fromJSON(us)));
+                    }
+                );
+            });
+    }
+
+    closeSprint(): void {
+        this.sprint.setState('archived');
+        this.sprintService.update(this.projectService.currentProject.getId(), this.sprint).subscribe(() => { });
+    }
 
 }
